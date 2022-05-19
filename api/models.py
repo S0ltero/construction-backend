ec8c19bd0a63b875f39686f8144f3b9ff8e22955
fastmed <maks.memelige@gmail.com>
2022-05-19 10:21:04 +0200

    Add `Element` model

--- api/models.py
+++ api/models.py
@@ -19,3 +19,19 @@ class SubCategory(models.Model):
     description = models.TextField(verbose_name="Описание")
     image = models.ImageField(verbose_name="Фото", blank=True)
     parent_category = models.ForeignKey(Category, verbose_name="Категория", on_delete=models.CASCADE, related_name="subcategories")
+
+
+class Element(models.Model):
+    class Type(models.TextChoices):
+        MATERIAL = "Material", "Материал"
+        JOB = "Job ", "Работа"
+
+    title = models.CharField(verbose_name="Название", max_length=60)
+    category = models.ForeignKey(Category, verbose_name="Категория", on_delete=models.CASCADE, related_name="elemnts")
+    measure = models.CharField(verbose_name="Единицы измерения", max_length=30)
+    second_measure = models.CharField(verbose_name="Вторая единица измерения", max_length=30)
+    cost = models.PositiveIntegerField(verbose_name="Цена", default=0)
+    price = models.PositiveIntegerField(verbose_name="Сумма", default=0)
+    type = models.CharField(verbose_name="Тип", choices=Type.choices, max_length=30)
+    dimension = models.CharField(verbose_name="Размер", max_length=60, blank=True)
+    conversion_rate = models.PositiveIntegerField(verbose_name="Коэффициент конверсии")
