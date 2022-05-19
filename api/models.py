85b871d14b6a48d39c1052fb932454f545d99526
fastmed <maks.memelige@gmail.com>
2022-05-19 10:20:30 +0200

    Add `Category` model

--- api/models.py
+++ api/models.py
@@ -3,3 +3,12 @@ from django.contrib.auth.models import AbstractUser
 
 
 # Create your models here.
+class Category(models.Model):
+    class Type(models.TextChoices):
+        CONSTRUCTION = "Construction", "Конструкция"
+        ELEMENT = "Element ", "Элемент"
+
+    title = models.CharField(verbose_name="Название", max_length=60)
+    description = models.TextField(verbose_name="Описание")
+    image = models.ImageField(verbose_name="Фото", blank=True)
+    type = models.CharField(verbose_name="Тип", choices=Type.choices, max_length=30)
