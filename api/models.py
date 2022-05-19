f64b0a76df11fd25c6486a29e00fd8f93ff672d8
fastmed <maks.memelige@gmail.com>
2022-05-19 10:20:41 +0200

    Add `SubCategory` model

--- api/models.py
+++ api/models.py
@@ -12,3 +12,10 @@ class Category(models.Model):
     description = models.TextField(verbose_name="Описание")
     image = models.ImageField(verbose_name="Фото", blank=True)
     type = models.CharField(verbose_name="Тип", choices=Type.choices, max_length=30)
+
+
+class SubCategory(models.Model):
+    title = models.CharField(verbose_name="Название", max_length=60)
+    description = models.TextField(verbose_name="Описание")
+    image = models.ImageField(verbose_name="Фото", blank=True)
+    parent_category = models.ForeignKey(Category, verbose_name="Категория", on_delete=models.CASCADE, related_name="subcategories")
