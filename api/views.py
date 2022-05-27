efbf0c555de8bf328f28f520daec7bc39fa7b02a
fastmed <maks.memelige@gmail.com>
2022-05-27 14:12:25 +0200

    Add imports for `Views`

--- api/views.py
+++ api/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+
+from .models import (
+    Category, SubCategory, Element,
+    Construction, Project, ProjectStage,
+    Template, TemplateStage, Client,
+    ProjectDocument
+)
+
+from . serializers import (
+    CategorySerializer, SubCategorySerializer, ElementSerializer,
+    ConstructionSerializer, ProjectSerializer, ProjectStageSerializer,
+    TemplateSerializer, TemplateStageSerializer, ClientSerializer,
+    ProjectDocumentSerializers
+)
+
 
 # Create your views here.
