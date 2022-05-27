429a7335ee5aaf89670a024f4b77fce1f8a9c148
fastmed <maks.memelige@gmail.com>
2022-05-27 14:08:53 +0200

    Add imports for serializers

---
+++ api/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from .models import (
+    Category, SubCategory, Element,
+    Construction, Project, ProjectStage,
+    Template, TemplateStage, Client,
+    ConstructionElement,
+    ProjectConstruction, TemplateConstruction,
+)
