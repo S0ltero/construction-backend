228fe46bb6ab9c0b8b5c06b05d660a475c8a8f38
fastmed <maks.memelige@gmail.com>
2022-05-27 14:16:37 +0200

    Add main urls

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,11 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 
+router = DefaultRouter()
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", include("api.urls", namespace="api")),
+    path("api/auth/", include("djoser.urls")),
+    path("api/auth/", include("djoser.urls.authtoken"))
 ]
