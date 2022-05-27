c41647a036f83de34058eb9c2ba7a8560ddeef29
fastmed <maks.memelige@gmail.com>
2022-05-27 14:12:57 +0200

    Add `SubCategoryViewSet`

--- api/views.py
+++ api/views.py
@@ -37,3 +37,18 @@ class CategoryViewSet(viewsets.GenericViewSet):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SubCategoryViewSet(viewsets.GenericViewSet):
+    queryset = SubCategory.objects.all()
+    serializer_class = SubCategorySerializer
+
+    def create(self, request):
+        # Создание подкатегории
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid(raise_exception=False):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
