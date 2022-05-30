02370a7d981c2c05321e2fc3d759e1b9a459b771
fastmed <maks.memelige@gmail.com>
2022-05-30 15:22:54 +0200

    Update imports for serializers

--- api/serializers.py
+++ api/serializers.py
@@ -2,10 +2,10 @@ from rest_framework import serializers
 
 from .models import (
     Category, SubCategory, Element,
-    Construction, Project, ProjectStage,
-    Template, TemplateStage, Client,
-    ConstructionElement,
-    ProjectConstruction, TemplateConstruction
+    Construction, ConstructionElement,
+    Project, ProjectStage, ProjectConstruction,
+    Template, TemplateStage, TemplateConstruction,
+    Client
 )
 
 
