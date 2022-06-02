9acb91c4548d92a3f024a0720268aa22687cfd50
fastmed <maks.memelige@gmail.com>
2022-06-02 15:12:08 +0200

    Add `subcategory` field to `Construction` model

--- api/models.py
+++ api/models.py
@@ -53,6 +53,13 @@ class Construction(models.Model):
     title = models.CharField(verbose_name="Название", max_length=60)
     measure = models.CharField(verbose_name="Единицы измерения", max_length=30)
     category = models.ForeignKey(Category, verbose_name="Категория", on_delete=models.CASCADE, related_name="constructions")
+    subcategory = models.ForeignKey(
+        SubCategory,
+        verbose_name="Подкатегория",
+        on_delete=models.CASCADE,
+        related_name="constructions",
+        null=True
+    )
 
 
 class ConstructionDocument(models.Model):
