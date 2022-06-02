0b186faea95be330f7d1030bad2c48cbe3a05c2e
fastmed <maks.memelige@gmail.com>
2022-06-02 14:47:53 +0200

    Move `CategorySerializer` down in the code

--- api/serializers.py
+++ api/serializers.py
@@ -11,15 +11,15 @@ from .models import (
 )
 
 
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = "__all__"
-
-
 class SubCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = SubCategory
+        fields = "__all__"
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
         fields = "__all__"
 
 
