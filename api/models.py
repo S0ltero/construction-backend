150422caa8f4d4af7834fbe45f452b2dd00d84f0
fastmed <maks.memelige@gmail.com>
2022-06-02 14:12:45 +0200

    Update `ProjectConstruction` model

--- api/models.py
+++ api/models.py
@@ -90,9 +90,10 @@ class ProjectStage(models.Model):
 
 
 class ProjectConstruction(models.Model):
+    title = models.CharField(verbose_name="Название", max_length=60)
     count = models.PositiveIntegerField(verbose_name="Номер проекта")
     stage = models.ForeignKey(ProjectStage, verbose_name="Стадия", on_delete=models.CASCADE, related_name="constructions")
-    construction = models.ForeignKey(Construction, verbose_name="Конструкция", on_delete=models.CASCADE)
+    measure = models.CharField(verbose_name="Единицы измерения", max_length=30)
 
 
 class Template(models.Model):
