5cb1dcfdd5120e5a57d509f2dbe1f03ce12ac939
S0ltero <AlexsSobko61@gmail.com>
2022-06-02 22:44:47 +0300

    Add `docs/` url to main `urls.py`

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 from rest_framework.routers import DefaultRouter
 
@@ -10,5 +11,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls", namespace="api")),
     path("api/auth/", include("djoser.urls")),
-    path("api/auth/", include("djoser.urls.authtoken"))
+    path("api/auth/", include("djoser.urls.authtoken")),
+    path("docs/", TemplateView.as_view(template_name="elements.html"))
 ]
