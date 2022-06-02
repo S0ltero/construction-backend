8c65c195bf0d1218f27ca0fc4ad7e341df48f024
fastmed <maks.memelige@gmail.com>
2022-06-02 15:11:17 +0200

    Fixed incorrect `related_name` for `Element` model

--- api/models.py
+++ api/models.py
@@ -27,7 +27,7 @@ class Element(models.Model):
         JOB = "Job ", "Работа"
 
     title = models.CharField(verbose_name="Название", max_length=60)
-    category = models.ForeignKey(Category, verbose_name="Категория", on_delete=models.CASCADE, related_name="elemnts")
+    category = models.ForeignKey(Category, verbose_name="Категория", on_delete=models.CASCADE, related_name="elements")
     measure = models.CharField(verbose_name="Единицы измерения", max_length=30)
     second_measure = models.CharField(verbose_name="Вторая единица измерения", max_length=30)
     cost = models.PositiveIntegerField(verbose_name="Цена", default=0)
