23c0fa3d2bd3e0b97a91a54819d4f866c280c66a
S0ltero <AlexsSobko61@gmail.com>
2022-06-14 19:34:27 +0300

    Add `cost` field to `ConstructionElementSerializer`

--- api/serializers.py
+++ api/serializers.py
@@ -43,6 +43,8 @@ class ElementSerializer(serializers.ModelSerializer):
 class ConstructionElementSerializer(serializers.ModelSerializer):
     measure = serializers.CharField(source="element.measure")
     price = serializers.IntegerField(source="element.price")
+    cost = serializers.IntegerField(source="element.cost")
+
     class Meta:
         model = ConstructionElement
         fields = "__all__"
