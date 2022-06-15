4be9bd9b122604910bd9c77e2ce024618eae0e89
S0ltero <AlexsSobko61@gmail.com>
2022-06-15 13:11:33 +0300

    Add `measure`, `price` and `cost` fields to `ProjectConstructionElementSerializer`

--- api/serializers.py
+++ api/serializers.py
@@ -105,6 +105,10 @@ class ProjectSerializer(serializers.ModelSerializer):
 
 
 class ProjectConstructionElementSerializer(serializers.ModelSerializer):
+    measure = serializers.CharField(source="element.measure")
+    price = serializers.IntegerField(source="element.price")
+    cost = serializers.IntegerField(source="element.cost")
+
     class Meta:
         model = ProjectConstructionElement
         fields = "__all__"
