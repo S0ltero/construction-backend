e6651f35259015144172ab699d7d96e064817d6a
S0ltero <AlexsSobko61@gmail.com>
2022-07-06 15:34:35 +0300

    Add `price` field to `Construction` model

--- api/models.py
+++ api/models.py
@@ -109,6 +109,7 @@ class Construction(models.Model):
     description = models.TextField(verbose_name="Описание", blank=True)
     measure = models.CharField(verbose_name="Единицы измерения", max_length=30)
     cost = models.PositiveIntegerField(verbose_name="Себестоимость", default=0)
+    price = models.PositiveIntegerField(verbose_name="Цена", default=0)
     parent_category = models.ForeignKey(ParentCategory, verbose_name="Родительская категория", on_delete=models.CASCADE, related_name="constructions")
     category = models.ForeignKey(
         Category,
