72f5c647e3e9a71f348f1ea60859b51504edf6bb
S0ltero <AlexsSobko61@gmail.com>
2022-10-28 12:37:41 +0300

    Rework `insert_cells` function - Now more readability - Now avoid useless nesting

--- api/excel.py
+++ api/excel.py
@@ -12,17 +12,10 @@ from django.core.exceptions import ValidationError
 
 def insert_cells(ws: Worksheet, ws_row: int, cells: dict):
     """Insert cell value on row of `ws_row`, getted by column letter from `cells`"""
-    for column in (ws.iter_cols(min_row=ws_row, min_col=1, max_col=8, max_row=ws_row)):
-        for cell in column:
-            letter = cell.column_letter
-            if not cells.get(cell.column_letter):
-                continue
-            cell.value = cells[letter]["value"]
-            cell.alignment = cells[letter].get("alignment", None)
-
-    ws_row += 1
-
-    return ws, ws_row
+    for key, value in cells.items():
+        ws[f"{key}{ws_row}"] = value.get("value")
+        ws[f"{key}{ws_row}"].alignment = value.get("alignment", None)
+    return ws, ws_row + 1
 
 
 def foreman(project):
